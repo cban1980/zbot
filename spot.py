69f55a6a8144dc86f2781db23bd72f5c74162dda
cban1980 <cban@gmx.com>
2021-12-08 23:06:32 +0100

    remove stuff

--- spot.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-#-*- coding: utf-8 -*-
-import requests
-from bs4 import BeautifulSoup as bs
-html = requests.get('https://open.spotify.com/track/135Lf4Q0CzlMNfOxbEUsLH?si=ca6c1a4981764c71').text
-soup = bs(html, 'html5lib')
-print(soup.title.string)
